d0da45fe9ac394df2b3290ce8bd39b908952b69f
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-01-06 09:59:49 +0700

    initialize project

---
+++ Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TestEntity> TestEntities { get; set; }
+    }
+}
